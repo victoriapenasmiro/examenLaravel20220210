a1207514ae9efecd5ea24ee874264fc9f95fbe17
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2022-02-10 18:05:56 +0100

    1 - completado

---
+++ victoriapenyas20220210/database/migrations/2022_02_10_165910_create_post_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 85);
+            $table->text('extracto');
+            $table->text('contenido');
+            $table->boolean('caducable')->default(0);
+            $table->boolean('comentable')->default(0);
+            $table->string('acceso', 15);
+            $table->date('fecha_publicacion');
+            $table->integer('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
